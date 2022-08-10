4a562a7529947cf37abb2a77a2669dda59ee8439
CindyOrtega416 <cindyortega416@gmail.com>
2022-08-10 20:12:44 -0300

    working on filter

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,14 +1,36 @@
 import React, { useState } from 'react';
 
+const categoryOption = [
+    "Perdido",
+    "Encontrado",
+    "Adopción"
+]
+
 export default function Filter({ blogs }) {
 
-    const [filteredAnimal, setFilteredAnimal] = useState([])
+    const [filteredCategory, setFilteredCategory] = useState([])
+
+
     return (
     
         <div>
             <div className="blog-heading text-start pt-3 py-2 mb-4">Filtrar</div>
+    
+            <div className="col-12 py-3">
+                    <select
+                        className="catg-dropdown"
+                    >
+                        <option>Categoria</option>
+                        {categoryOption.map((option, index) => (
+                            <option value={option || ""} key={index}>
+                                {option}
+                            </option>
+                        ))}
+                    </select>
+                </div>
             {
                 blogs?.map((item)=> {
+                    
 
                 })
             }
