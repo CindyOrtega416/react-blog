e0a50bd52e11ca16dc2e46c4299529b72b92f8db
Cindy Ortega <cindyortega416@gmail.com>
2022-09-06 03:23:54 -0300

    added pagination and some refactoring

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,6 +8,8 @@ import {
     orderBy,
     query, serverTimestamp,
     where,
+    limit,
+    startAfter
 } from "firebase/firestore";
 import { db } from "../firebase";
 import BlogSection from "../components/BlogSection";
@@ -23,9 +25,7 @@ export default function Home({ setActive, user }) {
     const [loading, setLoading] = useState(true)
     const [blogs, setBlogs] = useState([])
     const [filters, setFilters] = useState([])
-    const [tags, setTags] = useState([])
     const [trendBlogs, setTrendBlogs] = useState([])
-    const [categoryBlogs, setCategoryBlogs] = useState([])
 
     const [activeCategory, setActiveCategory] = useState('Todos')
     const [activeGender, setActiveGender] = useState('Todos')
@@ -34,6 +34,9 @@ export default function Home({ setActive, user }) {
     const [activeEyes, setActiveEyes] = useState('Todos')
     const [activeChip, setActiveChip] = useState('Todos')
     const [activeCollar, setActiveCollar] = useState('No')
+
+    let [lastDoc, setLastDoc] = useState()
+    let [firstDoc, setFirstDoc] = useState()
 
     const getTrendingBlogs = async () => {
         const blogRef = collection(db, "blogs")
@@ -47,66 +50,49 @@ export default function Home({ setActive, user }) {
 
     }
 
-    // console.log("activeCategory", activeCategory)
+/*--------------------------Get data - snapshot----------------------*/
 
-    /* const getCategoryBlogs = async () => {
-         const blogRef = collection(db, "blogs")
-         const categoryQuery = query(blogRef, where("category", "==", activeCategory))*/
+    const updateDocs = (snapshot) => {
+        let list = []
 
-    /*  const unsubscribe = onSnapshot(categoryQuery, (querySnapshot) => {
-          let categoryBlogs = [];
-          querySnapshot.forEach((doc) => {
-              categoryBlogs.push(doc.data())
-          })
-         
-      })*/
-    /*       const querySnapshot = await getDocs(categoryQuery)
-           let categoryBlogs = []
-           querySnapshot.forEach((doc) => {
-               categoryBlogs.push({id: doc.id, ...doc.data() })
-           }) 
-           setCategoryBlogs(categoryBlogs)
-           console.log('CategoryCladdifiedBlog: ', categoryBlogs)
-       }
-   
-   */
+        snapshot.docs.forEach((doc) => {
+            list.push({ id: doc.id, ...doc.data() })
+        })
+        lastDoc = snapshot.docs[snapshot.docs.length - 1 ]
 
-    //si esta seleccionado 'todos', como hago para que muestre todas las opciones? como hacer para que 
-    //interpretre 'marca todas las opciones'
-    /* if(activeCategory === 'Todos' && activeGender === 'Todos' && activeAnimal === 'Todos'){
-         
-     } */
+        list.sort((a, b) => (a.timestamp < b.timestamp) ? 1 : -1)
+
+        firstDoc = list[0]
+
+        setBlogs(list)
+        setFilters(list)
+        setLoading(false)
+        setActive("home")
+        setLastDoc(lastDoc)
+        setFirstDoc(firstDoc)
+
+    }
 
 
-    useEffect(() => {
+/*----------------------Conditionals and queries----------------------*/
+
+    const fetchData = () => {
 
         let q = null
-        let unsub = null;
 
         //00
         if (activeCategory !== 'Todos' && activeGender !== 'Todos') {
             q = query(
                 collection(db, "blogs"),
                 where("category", "==", activeCategory),
-                where("gender", "==", activeGender))
-            unsub = onSnapshot(
+                where("gender", "==", activeGender),
+                limit(5))
+
+            onSnapshot(
                 q,
                 (snapshot) => {
-                    let list = []
-                    let tags = []
-                    snapshot.docs.forEach((doc) => {
-                        tags.push(...doc.get("tags"))
-                        list.push({ id: doc.id, ...doc.data() })
-                    })
-                    const uniqueTags = [...new Set(tags)]
-                    list.sort((a, b) => (a.timestamp < b.timestamp) ? 1 : -1)
 
-                    setTags(uniqueTags)
-                    setBlogs(list)
-                    setFilters(list)
-                    setLoading(false)
-                    setActive("home")
-
+                    updateDocs(snapshot) //refactored code
 
                     console.log("Conditional Nº 1")
                 }, (error) => {
@@ -119,24 +105,13 @@ export default function Home({ setActive, user }) {
             q = query(
                 collection(db, "blogs"),
                 where("category", "==", activeCategory),
-                where("gender", "!=", activeGender))
-            unsub = onSnapshot(
+                where("gender", "!=", activeGender),
+                limit(5))
+
+            onSnapshot(
                 q,
                 (snapshot) => {
-                    let list = []
-                    let tags = []
-                    snapshot.docs.forEach((doc) => {
-                        tags.push(...doc.get("tags"))
-                        list.push({ id: doc.id, ...doc.data() })
-                    })
-                    const uniqueTags = [...new Set(tags)]
-                    list.sort((a, b) => (a.timestamp < b.timestamp) ? 1 : -1)
-
-                    setTags(uniqueTags)
-                    setBlogs(list)
-                    setFilters(list)
-                    setLoading(false)
-                    setActive("home")
+                    updateDocs(snapshot) //refactored code
 
                     console.log("Condition nº 2")
                 }, (error) => {
@@ -149,24 +124,13 @@ export default function Home({ setActive, user }) {
             q = query(
                 collection(db, "blogs"),
                 where("category", "!=", activeCategory),
-                where("gender", "==", activeGender))
-            unsub = onSnapshot(
+                where("gender", "==", activeGender),
+                limit(5))
+
+            onSnapshot(
                 q,
                 (snapshot) => {
-                    let list = []
-                    let tags = []
-                    snapshot.docs.forEach((doc) => {
-                        tags.push(...doc.get("tags"))
-                        list.push({ id: doc.id, ...doc.data() })
-                    })
-                    const uniqueTags = [...new Set(tags)]
-                    list.sort((a, b) => (a.timestamp < b.timestamp) ? 1 : -1)
-
-                    setTags(uniqueTags)
-                    setBlogs(list)
-                    setFilters(list)
-                    setLoading(false)
-                    setActive("home")
+                    updateDocs(snapshot)    //refactored code
 
                     console.log("Condition nº 3")
                 }, (error) => {
@@ -177,103 +141,140 @@ export default function Home({ setActive, user }) {
 
 
         } else {
-            /* You're going to have to pick one field and query that one then filter 
+            /* You're going to have to pick one field and query that one then filter
             out the other on the client side. Compound Queries documentation is firm on this one.
             */
-            // firebase no permite compound queries en distintos campos 
+            // firebase no permite compound queries en distintos campos
             //11
-            q = query(collection(db, "blogs"), orderBy("timestamp", "desc"))
-            unsub = onSnapshot(
+            q = query(
+                collection(db, "blogs"),
+                orderBy("timestamp", "desc"),
+                limit(5))
+
+            onSnapshot(
                 q,
                 (snapshot) => {
-                    let list = []
-                    let tags = []
-                    snapshot.docs.forEach((doc) => {
-                        tags.push(...doc.get("tags"))
-                        list.push({ id: doc.id, ...doc.data() })
-                    })
-                    const uniqueTags = [...new Set(tags)]
-                    setTags(uniqueTags)
-                    setBlogs(list)
-                    setFilters(list)
-                    setLoading(false)
-                    setActive("home")
+                    updateDocs(snapshot)
 
                     console.log("Condition nº 4")
+                    console.log("A ver el last doc", lastDoc)
                 }, (error) => {
                     console.log(error)
                 }
             )
+
+            //  lastDoc = documentSnapshot.docs[]
+
+
         }
+    }
+
+/*----------------------Rendering data-------------------------------*/
+
+
+    useEffect(() => {
+        fetchData()
 
     }, [activeCategory, activeGender])
 
 
 
+/*--------------------------Next Page----------------------------*/
+
+    const nextPage = () => {
+        let q = null
+
+        //00
+        if (activeCategory !== 'Todos' && activeGender !== 'Todos') {
+            q = query(
+                collection(db, "blogs"),
+                where("category", "==", activeCategory),
+                where("gender", "==", activeGender),
+                startAfter(lastDoc),
+                limit(5))
+
+            onSnapshot(
+                q,
+                (snapshot) => {
+
+                    updateDocs(snapshot) //refactored code
+
+                    console.log("Conditional Nº 1")
+                }, (error) => {
+                    console.log(error)
+                }
+
+            )
+            //01
+        } else if (activeCategory !== 'Todos' && activeGender === 'Todos') {
+            q = query(
+                collection(db, "blogs"),
+                where("category", "==", activeCategory),
+                where("gender", "!=", activeGender),
+                startAfter(lastDoc),
+                limit(5))
+
+            onSnapshot(
+                q,
+                (snapshot) => {
+                    updateDocs(snapshot) //refactored code
+
+                    console.log("Condition nº 2")
+                }, (error) => {
+                    console.log(error)
+                }
+
+            )
+            //10
+        } else if (activeCategory === 'Todos' && activeGender !== 'Todos') {
+            q = query(
+                collection(db, "blogs"),
+                where("category", "!=", activeCategory),
+                where("gender", "==", activeGender),
+                startAfter(lastDoc),
+                limit(5))
+
+            onSnapshot(
+                q,
+                (snapshot) => {
+                    updateDocs(snapshot)    //refactored code
+
+                    console.log("Condition nº 3")
+                }, (error) => {
+                    console.log(error)
+                }
+
+            )
 
 
+        } else {
 
-    /*useEffect(() => {
-        getTrendingBlogs()
-        const unsub = onSnapshot(
-            query(collection(db, "blogs"), orderBy("timestamp", "desc")),
-            (snapshot) => {
-                let list = []
-                let tags = []
-                snapshot.docs.forEach((doc) => {
-                    tags.push(...doc.get("tags"))
-                    list.push({id: doc.id, ...doc.data()})
-                })
-                const uniqueTags = [...new Set(tags)]
-                setTags(uniqueTags)
-                setBlogs(list)
-                setFilters(list)
-                setLoading(false)
-                setActive("home")
- 
-            }, (error) => {
-                console.log(error)
-            }
-        )
- 
-        return () => {
-            unsub()
-            getTrendingBlogs()
-        } */
-    //  getCategoryBlogs()
+            q = query(
+                collection(db, "blogs"),
+                orderBy("timestamp", "desc"),
+                startAfter(lastDoc),
+                limit(5))
 
-    /*      const unsub = onSnapshot(
-              query(collection(db, "blogs"), (activeCategory !== 'Todos') ? where("category", "==", activeCategory) : null),
-              (snapshot) => {
-                  let list = []
-                  let tags = []
-                  snapshot.docs.forEach((doc) => {
-                      tags.push(...doc.get("tags"))
-                      list.push({ id: doc.id, ...doc.data() })
-                  })
-                  const uniqueTags = [...new Set(tags)]
-                  setTags(uniqueTags)
-                  setBlogs(list)
-                  setFilters(list)
-                  setLoading(false)
-                  setActive("home")
-  
-              }, (error) => {
-                  console.log(error)
-              }
-          )
-  
-          return () => {
-              unsub()
-              // getCategoryBlogs()
-          }
-  
-      }, [activeCategory])
-  */
+            onSnapshot(
+                q,
+                (snapshot) => {
+                    updateDocs(snapshot)
+
+                    console.log("Condition nº 4")
+                    console.log("A ver el last doc", lastDoc)
+                }, (error) => {
+                    console.log(error)
+                }
+            )
+        }
+    }
+
+/*-----------------------------------------------------------------*/
 
     if (loading) {
         return <Spinner />
     }
+
 
     const handleDelete = async (id) => {
         if (window.confirm("Are you sure you want to delete the blog?")) {
@@ -288,6 +289,8 @@ export default function Home({ setActive, user }) {
         }
     }
 
+
+    console.log("last doc 1", lastDoc)
     console.log("blogs", blogs)
 
     return (
@@ -302,6 +305,8 @@ export default function Home({ setActive, user }) {
                             user={user}
                             handleDelete={handleDelete}
                         />
+                        <button>Anterior</button>
+                        <button onClick={nextPage}>Siguiente</button>
                     </div>
                     <div className="col-md-3">
                         <Filter
@@ -326,7 +331,7 @@ export default function Home({ setActive, user }) {
                             eyesType={eyesType}
                             idCollar={idCollar}
                         />
-                        <Tags tags={tags} />
+                      {/*  <Tags tags={tags} />*/}
                         <MostPopular blogs={blogs} />
                     </div>
                 </div>
