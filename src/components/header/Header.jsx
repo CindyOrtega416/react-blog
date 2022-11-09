916aa0251eaeecd367c34b8b738d25f97ff984fe
Cindy Ortega <cindyortega416@gmail.com>
2022-11-09 18:31:48 -0300

    changed frontend image

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import './header.css'
 
 export default function Header() {
-    return(
+    return (
         <div className="header">
             <div className="headerTitles">
-                <span className="headerTitleSm">React & Node</span>
-                <span className="headerTitleLg">Blog</span>
+                <span className="headerTitleSm"></span>
+                <span className="headerTitleLg"></span>
             </div>
             <img
                 className="headerImg"
-                 src="https://i.pinimg.com/564x/5e/68/ba/5e68ba603a1ee8d3ee836cc49729cac9.jpg"
+                src="/images/icon.png"
             />
         </div>
     )
