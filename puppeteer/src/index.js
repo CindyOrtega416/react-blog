bfc995a1e5a1ff3ee4cf3fb8b38e178b9bd66593
Cindy Ortega <cindyortega416@gmail.com>
2022-11-11 01:05:39 -0300

    working with puppeteer

--- puppeteer/src/index.js
+++ puppeteer/src/index.js
@@ -1,9 +1,11 @@
 const data = require("./data");
 const {pageURL} = data;
+const dotenv = require('dotenv');
 
 const webscraping = require("./webscraping");
 const compareAndSaveResults = require("./resultAnalysis");
 
+dotenv.config();
 webscraping(pageURL)
     .then(dataObj => {
         compareAndSaveResults(dataObj);
--- puppeteer/src/resultAnalysis.js
+++ puppeteer/src/resultAnalysis.js
@@ -1,17 +1,57 @@
 const data = require("./data");
 const mongoose = require("mongoose");
 const axios = require("axios");
-/*
+const {disconnect} = require("mongoose");
+
 mongoose
     .connect(process.env.MONGO_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
     .then(console.log("Connected to MongoDB"))
-    .catch((err) => console.log(err));*/
+    .catch((err) => console.log(err));
+
 const compareAndSaveResults = async dataObj => {
     try {
-        await axios.post("/posts/", dataObj);
+        const Post = require('../models/Post')
+
+        Post.find({}, function (err, postsList) {
+            return postsList;
+        })
+            .then(postsList => {
+                if (postsList == "") {
+                    console.log(`A new data was created:\n${JSON.stringify(dataObj)}`);
+                    const newPostArray = new Post(dataObj);
+                    return newPostArray.save().catch(err => console.log(err))
+                }
+                const {hiddenId, posts} = dataObj;
+
+                const dbId = postsList[0]._id;
+                const dbHiddenId = postsList[0].hiddenId;
+                const dbPosts = postsList[0].posts;
+
+                let catchDifference = false;
+
+                if (dbHiddenId !== hiddenId) {
+                    catchDifference = true;
+                } else {
+                    dbPosts.forEach((post, i) => {
+                        if (post != posts[i]) catchDifference = true
+                    })
+                }
+
+                if (catchDifference) {
+                    console.log("A new evidence was found, updating database...");
+                    mongoose.set('useFindAndModify', false);
+                    return Post.findOneAndUpdate({_id: dbId}, dataObj);
+                }
+                console.log("File is equal to page, no news to report");
+            })
+            .then(() => {
+                mongoose.disconnect()
+            })
+            .catch(err => console.log(err))
+
     } catch (err) {
         console.log(err)
     }
